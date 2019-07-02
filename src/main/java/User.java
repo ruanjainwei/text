e953343b179e7dd8f8c7abb1a958afb21508a6f4
ruanjainwei <1242740489@qq.com>
2019-07-02 17:29:01 +0800

    添加java文件夹

---
+++ src/main/java/User.java
@@ -0,0 +1,10 @@
+/**
+ * created by Savage
+ * 2019/7/2 current system date
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("我要上传git");
+    }
+
+}
